359d0796495e8135b2df06d294ce5da1d52020c5
Simon Rovder <simon.rovder@gmail.com>
2017-12-09 17:35:11 +0000

    Adding validation

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask, g, request, Response, render_template
 from flask_cors import CORS
 from .universal_handler import UniversalHandler
 from time import time
+from .validation import ValidationException, Schema
 
 app = Flask(__name__, template_folder='templates')
 CORS(app)
 Handler = UniversalHandler(app)
+
+
+@app.errorhandler(ValidationException)
+def validation_error_handler(e):
+    return Response(render_template('400.html', message=str(e)), status=400)
 
 
 @Handler('/handler_test', methods=['GET'])
@@ -26,3 +32,13 @@ def content_get():
 @Handler('/template_test', methods=['GET'])
 def template_test_get():
     return render_template("template_test.html", stamp=int(time()))
+
+
+@Handler('/validation/<stuff>', methods=['GET'], args_schema=Schema(cast=dict, schema={
+    "a": Schema(cast=int, bounds=(0, 10), optional=True, default=8)
+}), urla_schema=Schema(cast=dict, schema={
+    "stuff": Schema(cast=str, length=(3, 16))
+}))
+def validation_get():
+    print(g.args['a'], g.urla['stuff'])
+    return 200
---
+++ backend/app/validation.py
@@ -0,0 +1,95 @@
+import re
+
+
+class ValidationException(Exception):
+    def __init__(self, key, message):
+        super(ValidationException, self).__init__()
+        self._key = key
+        self._message = message
+
+    def __str__(self):
+        return "Failed to validate %s: %s" % (self._key, self._message)
+
+
+class Schema:
+    def __init__(self, cast, length=None, bounds=None, optional=False, nullable=False, schema=None, regex=None,
+                 values=None, default=None):
+        self._cast = cast
+        self._length = length
+        self._bounds = bounds
+        self._optional = optional
+        self._nullable = nullable
+        self._schema = schema
+        self._regex = re.compile(regex) if regex else None
+        self._values = values
+        self._default = default
+
+    def optional(self):
+        return self._optional
+
+    def default(self):
+        return self._default
+
+    def validate(self, key, value):
+
+        if value is None:
+            if not self._nullable:
+                raise ValidationException(key, "Value cannot be null!")
+            return None
+
+        try:
+            value = self._cast(value)
+        except:
+            raise ValidationException(key, "Failed to cast %s as %s" % (value, str(self._cast)))
+
+        if self._length is not None:
+            l = len(value)
+            if self._length[0] is not None and l < self._length[0]:
+                raise ValidationException(
+                    key,
+                    "Must not be shorter than %s. Received length: %s" % (self._length[0], l)
+                )
+            if self._length[1] is not None and l > self._length[1]:
+                raise ValidationException(
+                    key,
+                    "Must not be longer than %s. Received length: %s" % (self._length[1], l)
+                )
+
+        if self._bounds is not None:
+            if self._bounds[0] is not None and value < self._bounds[0]:
+                raise ValidationException(
+                    key,
+                    "Must be at least %s. Received: %s" % (self._bounds[0], value)
+                )
+            if self._bounds[1] is not None and value > self._bounds[1]:
+                raise ValidationException(
+                    key,
+                    "Must be at most %s. Received %s" % (self._bounds[1], value)
+                )
+
+        if self._values is not None and value not in self._values:
+            raise ValidationException(
+                key,
+                "Value '%s' is not among the allowed values. Allowed values: %s" %
+                (value, ", ".join(["'%s'" % v for v in self._values]))
+            )
+
+        if self._cast == list:
+            return [self._schema.validate(key + ("[%i]" % i), item) for i, item in enumerate(value)]
+
+        if self._cast == dict:
+            validated = dict()
+            for k, v in self._schema.items():
+                if k not in value:
+                    if not v.optional():
+                        raise ValidationException(key + ("['%s']" % k), "Key is missing!")
+                    if v.default() is not None:
+                        validated[k] = v.default()
+                else:
+                    validated[k] = v.validate(key + ("['%s']" % k), value[k])
+            return validated
+
+        if self._regex and not re.match(self._regex, value):
+            raise ValidationException(key, "Value does not match the required regular expression.")
+
+        return value
