9be0217702731984e4273ac75142b7c8e0faba36
Simon Rovder <simon.rovder@gmail.com>
2017-12-10 10:36:31 +0000

    Adding deployment check

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,6 @@
 import socket
 from app import app
 
-host = "104.236.253.108" if socket.gethostname() == "ubuntu-512mb-nyc3" else "127.0.0.1"
+host = "104.236.253.108" if socket.gethostname() == "ubuntu-512mb-nyc3-01" else "127.0.0.1"
 
 app.run(host=host, port=5000, debug=True, threaded=True)
