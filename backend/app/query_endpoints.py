52f92e67988c54d35d80315205958bbf7a2c5b7e
root <s1350137@sms.ed.ac.uk>
2018-01-06 18:39:13 +0000

    Fixed css for slides

--- backend/app/query_endpoints.py
+++ backend/app/query_endpoints.py
@@ -3,7 +3,7 @@ from .rocchio import search_query, feedback_terms
 from .indexing import slides_index
 
 
-RESULTS_PER_PAGE = 20
+RESULTS_PER_PAGE = 18
 
 
 @Handler('/q/search', methods=['POST'], data_schema=Schema(cast=dict, schema={
@@ -19,7 +19,7 @@ def search_get():
     mode = g.data['mode']
     page = g.data['page']
     scores = search_query(g.data['query'], g.data['smart'])
-    sorted_scores = sorted(((score, uuid) for score, uuid in scores if score > 0), reverse=True)
+    sorted_scores = sorted(((score, uuid) for score, uuid in scores ), reverse=True)
     num_results = len(sorted_scores)
     if mode == 'pages':
        results = [
