051c7af087d954b2b2dfb85f00b651b41d6cc213
SimonRovder <simon.rovder@gmail.com>
2018-01-07 14:27:37 +0000

    Renaming endpoints

--- backend/app/query_endpoints.py
+++ backend/app/query_endpoints.py
@@ -7,58 +7,72 @@ from .tfidf import tfidf_test_instance
 RESULTS_PER_PAGE = 18
 
 
-@Handler('/q/search', methods=['POST'], data_schema=Schema(cast=dict, schema={
-    'mode': Schema(cast=str, values=('slides', 'pages')),
+@Handler('/search/pages', methods=['POST'], data_schema=Schema(cast=dict, schema={
     'query': Schema(cast=str, length=(3, 100)),
     'smart': SMART_SCHEMA,
     'page': Schema(cast=int, bounds=(0, 100))
 }))
-def search_get():
+def search_pages_post():
     """
     Query search with optional relevance feedback.
     """
-    mode = g.data['mode']
-    page = g.data['page']
     scores = search_query(g.data['query'], g.data['smart'])
     sorted_scores = sorted(((score, uuid) for score, uuid in scores if score > 0), reverse=True)
     num_results = len(sorted_scores)
-    if mode == 'pages':
-       results = [
-           dict(
-               uuid = uuid,
-               tfidf = {'word1': 0.7071067811865475, 'word2': 0.5071067811865475},
-               score = score,
-               title = uuid,
-               preview = get_preview(uuid, g.data['query']),
-               url = pages_index.get_url(uuid)
-           )
-           for score, uuid in sorted_scores[page * RESULTS_PER_PAGE:(page + 1) * RESULTS_PER_PAGE]
-       ]
 
-    elif mode == 'slides':
-        results = [
-           dict(
-               uuid = uuid,
-               score = score,
-               title = uuid,
-               content = 'Text here with highlights',
-               url = slides_index.get_url(uuid)
-           )
-           for score, uuid in sorted_scores[page * RESULTS_PER_PAGE:(page + 1) * RESULTS_PER_PAGE]
-       ]
+    results = [
+        dict(
+            uuid = uuid,
+            tfidf = {'word1': 0.7071067811865475, 'word2': 0.5071067811865475},
+            score = score,
+            title = uuid,
+            preview = get_preview(uuid, g.data['query']),
+            url = pages_index.get_url(uuid)
+        )
+        for score, uuid in sorted_scores[g.data['page'] * RESULTS_PER_PAGE:(g.data['page'] + 1) * RESULTS_PER_PAGE]
+    ]
+
     return {
         'pageCount': num_results // RESULTS_PER_PAGE + (num_results % RESULTS_PER_PAGE > 0),
         'results': results,
     }
 
 
-@Handler('/q/feedback', methods=['POST'], data_schema=Schema(cast=dict, schema={
+@Handler('/search/slides', methods=['POST'], data_schema=Schema(cast=dict, schema={
+    'query': Schema(cast=str, length=(3, 100)),
+    'smart': SMART_SCHEMA,
+    'page': Schema(cast=int, bounds=(0, 100))
+}))
+def search_slides_post():
+    """
+    Query search with optional relevance feedback.
+    """
+    scores = search_query(g.data['query'], g.data['smart'])
+    sorted_scores = sorted(((score, uuid) for score, uuid in scores if score > 0), reverse=True)
+    num_results = len(sorted_scores)
+    results = [
+       dict(
+           uuid = uuid,
+           score = score,
+           title = uuid,
+           content = 'Text here with highlights',
+           url = slides_index.get_url(uuid)
+       )
+       for score, uuid in sorted_scores[g.data['page'] * RESULTS_PER_PAGE:(g.data['page'] + 1) * RESULTS_PER_PAGE]
+    ]
+    return {
+        'pageCount': num_results // RESULTS_PER_PAGE + (num_results % RESULTS_PER_PAGE > 0),
+        'results': results,
+    }
+
+
+@Handler('/search/feedback', methods=['POST'], data_schema=Schema(cast=dict, schema={
     'query': Schema(cast=str, length=(3, 100)),
     'docs': Schema(cast=list, length=(0, 100),
                    schema=Schema(cast=str, regex='[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}')),
     'smart': SMART_SCHEMA,
 }))
-def feedback_get():
+def search_feedback_post():
     """
     Retrieve most relevant terms from documents.
     """
