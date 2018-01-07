50171be74db56d4de6c9a5dcd821cfda621aa351
Emilia <bogdanova.emily@gmail.com>
2018-01-07 13:00:41 +0000

    Highlights added to frontend (#48)
    * Fixed css for slides
    
    * New search goes to page 1
    
    * Highlighting frontend
    
    * Added highlighting for frontend
    
    * Make strings consistent
    
    * Changed title color in slides too
    
    * Endpoint imports fixed in different commit.

--- frontend/src/PageResults.js
+++ frontend/src/PageResults.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './PageResults.css';
 import WordScorePanel from './WordScorePanel';
 
-const Page = ({data}) => (
+var Highlighter = require('react-highlight-words');
+
+const Page = ({data, preview}) => (
   <div className="Page" data-uuid={data.uuid}>
     <a className="Page-link" href={data.url} target="_blank"> 
       <div className="Page-title">{data.title}</div>
       <div className="Page-url">{data.url}</div>
-      <div className="Page-content">{data.preview.preview}</div>
+      <div className="Page-content"><Highlighter
+        highlightClassName="Highlight"
+        searchWords={preview.highlight}
+        autoEscape={true}
+        textToHighlight={preview.preview}/>
+      </div>
     </a>
     <WordScorePanel results={data.tfidf} isOpened={false} />
   </div>
@@ -20,7 +27,7 @@ class PageResults extends React.Component {
     return (
       <div className="PageResults">
         {results.map((data) => (
-          <Page key={data.uuid} data={data}/>
+          <Page key={data.uuid} data={data} preview={data.preview}/>
         ))}
       </div>
     )
