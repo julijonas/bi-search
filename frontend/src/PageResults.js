11f2395b893dd0a9d1185ccbc159c3385ac7c15c
Julijonas Kikutis <julijonas.kikutis@gmail.com>
2018-01-08 20:06:00 +0000

    frontend: Add missing info like in a popular search engine

--- frontend/src/PageResults.js
+++ frontend/src/PageResults.js
@@ -15,16 +15,19 @@ const Page = ({data}) => (
           autoEscape={true} textToHighlight={data.preview.preview}/>
       </div>
     </NewTabLink>
-    <WordScorePanel results={data.tfidf} text="TF-IDF term weights for this document" isOpened={false} />
+    <WordScorePanel results={data.tfidf} missingText="Missing"
+                    text="TF-IDF term weights for this document" isOpened={false} />
   </div>
 );
 
 class PageResults extends React.Component {
   render() {
     const {results, queryWeights} = this.props;
+
     return (
       <div className="PageResults">
-        <WordScorePanel results={queryWeights} text="overall TF-IDF term weights" isOpened={false} />
+        <WordScorePanel results={queryWeights} missingText="Tokens not in index"
+                        text="overall TF-IDF term weights" isOpened={false} />
         {results.map((data) => (
           <Page key={data.uuid} data={data}/>
         ))}
--- frontend/src/WordScorePanel.js
+++ frontend/src/WordScorePanel.js
@@ -4,7 +4,7 @@ import './WordScorePanel.css';
 
 const WordScore = ({word, score}) => (
   <div className="WordScore" style={{width: `${score * 100}%`}}>
-    <div className="WordScore-score">{word}: {score.toFixed(3)}</div>
+    <div className="WordScore-score">{word}:&nbsp;{score.toFixed(3)}</div>
   </div>
 );
 
@@ -22,18 +22,30 @@ class WordScorePanel extends React.Component {
   };
 
   render() {
-    const {results, text} = this.props;
+    const {results, text, missingText} = this.props;
+
+    const missing = Object.keys(results).filter((word) => results[word] === 0);
+    const available = Object.keys(results).filter((word) => results[word] > 0);
 
     return (
       <div className="WordScorePanel">
+        {missing.length ? (
+          <div className="WordScorePanel-missing">{missingText}:{missing.map((word) => (
+            <React.Fragment>
+              {' '}<s>{word}</s>
+            </React.Fragment>
+          ))}
+          </div>
+        ) : null}
+
         <a onClick={this.handleChange} className="WordScorePanel-toggle">
           {this.state.isOpened ? 'Hide' : 'Show'} {text}
         </a>
 
-        <Collapse isOpened={this.state.isOpened} fixedHeight={results.length * 36} >
+        <Collapse isOpened={this.state.isOpened}>
           <div className="WordScorePanel-scores">
-            {Object.keys(results).map((key) => (
-               <WordScore word={key} score={results[key]} key={key} />
+            {available.map((word) => (
+               <WordScore word={word} score={results[word]} key={word} />
             ))}
           </div>
         </Collapse>
